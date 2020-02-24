e737bb5469908ca115c2853d07df4a0a752d560d
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-24 14:39:38 +0900

    GamerSquidのインポート
    日本のライトのインポート

---
+++ Assets/Scripts/MovingPlanController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlanController : MonoBehaviour
+{
+    [SerializeField] float power = 5.0f;  //移動させる力
+    Vector3 objPosition;
+    void Start()
+    {
+        objPosition = this.transform.position;  //初期位置
+    }
+
+    void Update()
+    {
+        // this.transform.position = new Vector3(
+        //     objPosition.x, objPosition.y, objPosition.z + Mathf.Sin(Time.time) * power );
+        this.transform.position = new Vector3(
+            objPosition.x, objPosition.y + Mathf.Sin(Time.time) * power, objPosition.z );
+    }
+}
