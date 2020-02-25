d6289292a68ca19926cb233863f0820f974b9877
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-26 02:47:20 +0900

    MovingLightの作成

---
+++ Assets/Scripts/MovingLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLight : MonoBehaviour
+{
+    [SerializeField] float power = 0.5f;
+    Vector3 objPosition;
+    void Start()
+    {
+        objPosition = this.transform.position;
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector3(
+            objPosition.x, objPosition.y + Mathf.Sin(Time.time) * power, objPosition.z);
+    }
+}
