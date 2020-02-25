92156d1e6f621cb09081bcd5ccef03d7fb925651
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-26 01:55:34 +0900

    thirdStageのヒント追加

---
+++ Assets/Scripts/HintBoard/HintBoard4.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HintBoard4: MonoBehaviour
+{
+
+    public GameObject hintText;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Hand")
+        {
+            hintText.GetComponent<Text>().text = "鈍感なゾンビでも、前には立たないように気をつけて..";
+        }
+    }
+
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     if(other.gameObject.tag == "Hand")
+    //     {
+    //         hintText.GetComponent<Text>().text = "彼に話しかけてみてよ...";
+    //     }
+    // }
+}
