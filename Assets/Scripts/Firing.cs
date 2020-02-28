4e84b0ba8792748b2aafdb126ed260d6053a976f
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-28 13:15:23 +0900

    Fire effctの追加

--- Assets/Scripts/Firing.cs
+++ Assets/Scripts/Firing.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Firing : MonoBehaviour
 {
-    // public GameObject fire;
+    public GameObject fire;
     void Start()
     {
         
@@ -21,8 +21,16 @@ public class Firing : MonoBehaviour
         if(other.gameObject.tag == "Fire")
         {
             Destroy(other.transform.parent.gameObject);
-            Debug.Log("発火したよ");
-            // Instantiate(fire, gameObject.transform, Quaternion.identity);
+            // Debug.Log("発火したよ");
+            Instantiate(fire, gameObject.transform.position, Quaternion.identity);
+            Invoke("endFire", 5.0f);
         }
     }
+
+    public void endFire()
+    {
+        Destroy(fire);
+    }
 }
+
+
