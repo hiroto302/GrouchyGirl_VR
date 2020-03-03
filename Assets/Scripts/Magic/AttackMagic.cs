e4cc997b0b1db7c7ed4619e20896f9c8571b111e
hiroto302 <i.am.a.hiroto@gmail.com>
2020-03-03 14:59:16 +0900

    魔法攻撃機能追加

---
+++ Assets/Scripts/Magic/AttackMagic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMagic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Breakable")
+        {
+            Debug.Log("Breakableにふれたよ");
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.tag == "SampleZombie")
+        {
+            Debug.Log("SampleZombieにふれたよ");
+            // Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Magic/AttackedByMagic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackedByMagic : MonoBehaviour
+{
+    public GameObject damageEffect;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "FireMagic")
+        {
+            Debug.Log("魔法をくらったよ");
+            Instantiate(damageEffect, transform.position, Quaternion.identity);
+            animator.SetTrigger("Damage");
+            Invoke("DestroyZombie", 3.0f);
+        }
+    }
+
+    public void DestroyZombie()
+    {
+        Destroy(gameObject);
+    }
+}
