153886e75d27c1e5f742db747b413121804887ed
hiroto302 <i.am.a.hiroto@gmail.com>
2020-03-03 13:16:33 +0900

    両手から魔法発動可能機能追加
    両手一同に発生させる

---
+++ Assets/Scripts/MagicController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicController : MonoBehaviour
+{
+    public GameObject magicPrefab;
+    public bool magic;
+
+    void Start()
+    {
+        magic = false;
+    }
+
+    void Update()
+    {
+        if(magic == true)
+        {
+            //右手操作
+            if(OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.A))
+            {
+                OVRInput.SetControllerVibration(0.3f, 0.3f, OVRInput.Controller.RTouch);
+                Instantiate(magicPrefab, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+                Debug.Log("右から発生");
+
+            }
+            else if(OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger))
+            {
+                OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+            }
+
+            //左手操作
+            if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.S))
+            {
+                OVRInput.SetControllerVibration(0.3f, 0.3f, OVRInput.Controller.LTouch);
+                Instantiate(magicPrefab, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+                Debug.Log("左から発生");
+            }
+            else if(OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+            }
+        }
+    }
+}
